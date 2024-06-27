d1acf2ddffa627a02fa297269ef65779226026da
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-06-27 15:34:42 +0200

    Added minimal package files

---
+++ src/pCRscore/discovery_svm.py
@@ -0,0 +1,5 @@
+def preprocess(data):
+    # TODO: replace pass below with code from 
+    # https://github.com/YounessAzimzade/XML-TME-NAC-BC/blob/main/Discovery%20SVM.ipynb
+    # section "Preprocessing"
+    pass
