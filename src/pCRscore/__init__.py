045581176b6d40abbb7a0f7c89aa77289637ff8e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-06-28 09:37:43 +0200

    Added `pCRscore.__version__`
    Takes info from `pyproject.toml`.

---
+++ src/pCRscore/__init__.py
@@ -0,0 +1,12 @@
+try:
+    # Try to fetch the version for installed packages
+    from importlib.metadata import version
+    __version__ = version("pCRscore")
+except ImportError:
+    # Fallback for Python<3.8 where importlib.metadata might not be available
+    try:
+        from importlib_metadata import version
+        __version__ = version("pCRscore")
+    except ImportError:
+        # Set a default version or leave it undefined if you prefer
+        __version__ = "unknown"
