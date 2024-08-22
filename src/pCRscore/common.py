af33c5325316e4592664587d93dbe70fcb25c150
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-08-22 06:50:06 +0200

    Add arg to preprocess(), test for validation (#1)

--- src/pCRscore/common.py
+++ src/pCRscore/common.py
@@ -1,7 +1,7 @@
 
 import pandas
 
-def preprocess(data):
+def preprocess(data, svm_type = "discovery"):
     # Mapping the values in the 'Response' column to binary values 0 and 1
     resp = {'pCR': 1, 'RD': 0}
     data.Response = [resp[item] for item in data.Response]
@@ -15,10 +15,17 @@ def preprocess(data):
     data = pandas.get_dummies(data, columns=categorical_cols)
 
     # Selecting validation cohort data
-    valid_cohort = [
-        'E-MTAB-4439', 'GSE18728', 'GSE19697', 'GSE20194', 'GSE20271',
-        'GSE22093', 'GSE22358', 'GSE42822', 'GSE22513'
-    ]
+    if svm_type == "discovery":
+        valid_cohort = [
+            'E-MTAB-4439', 'GSE18728', 'GSE19697', 'GSE20194', 'GSE20271',
+            'GSE22093', 'GSE22358', 'GSE42822', 'GSE22513'
+        ]
+    elif svm_type == "validation":
+        valid_cohort = [
+            'GSE25066', 'GSE32603', 'GSE32646', 'GSE37946', 'GSE50948',
+            'GSE23988'
+        ]
+
     data = data[data['Trial'].isin(valid_cohort)]
 
     return data
