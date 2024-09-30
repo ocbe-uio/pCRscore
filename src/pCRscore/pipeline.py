674df68ddbc6a308fc81024b44811204d530414a
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-30 11:39:06 +0200

    normalize_data() allows filtering of columns (#1)

--- src/pCRscore/pipeline.py
+++ src/pCRscore/pipeline.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # Function to normalize cell fractions and remove outliers
-def normalize_data(data):
+def normalize_data(data, cols=None):
+    if cols is not None:
+        data = data.iloc[:, cols]
     data = data.apply(pd.to_numeric, errors='coerce')
     for i in range(data.shape[1]):
         data.iloc[:, i] /= data.iloc[:, i].quantile(0.99)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -5,14 +5,14 @@ import math
 
 # Import DiscoveryData.csv and keep columns 4 to 39
 data = pandas.read_csv(".meta/DiscoveryData.csv")
-data = data.iloc[:, 3:38]
 
 # Normalize data
-data_norm = pipeline.normalize_data(data.copy())
+data_norm = pipeline.normalize_data(data.copy(), range(3, 39))
 
 # Test
 def test_normalize_data():
-  assert data_norm.shape == data.shape
+  assert data_norm.shape[0] == data.shape[0]
+  assert data_norm.shape[1] == data.shape[1] - 11
   tol = 1e-3
   assert math.isclose(data['B.cells.Memory'].iloc[0], 0.01789, rel_tol=tol)
   assert math.isclose(data_norm['B.cells.Memory'].iloc[0], 0.34186, rel_tol=tol)
