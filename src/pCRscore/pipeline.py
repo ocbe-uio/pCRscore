535267bfdaf02c9034e865b79a0e4e0aabdb3417
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-30 13:39:26 +0200

    Replace `pd` with `pandas` (#1)

--- src/pCRscore/pipeline.py
+++ src/pCRscore/pipeline.py
@@ -1,10 +1,10 @@
-import pandas as pd
+import pandas
 
 # Function to normalize cell fractions and remove outliers
 def normalize_data(data, cols=None):
     if cols is not None:
         data = data.iloc[:, cols]
-    data = data.apply(pd.to_numeric, errors='coerce')
+    data = data.apply(pandas.to_numeric, errors='coerce')
     for i in range(data.shape[1]):
         data.iloc[:, i] /= data.iloc[:, i].quantile(0.99)
     return data
