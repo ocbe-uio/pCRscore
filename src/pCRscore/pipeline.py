6e82fd11cfeefb213589c7950de4e1f0297c23df
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-30 13:42:17 +0200

    Dropped coersion to float (#1)
    Instead, function should (silently?) drop columns that are not numeric

--- src/pCRscore/pipeline.py
+++ src/pCRscore/pipeline.py
@@ -2,9 +2,9 @@ import pandas
 
 # Function to normalize cell fractions and remove outliers
 def normalize_data(data, cols=None):
+    # TODO: drop columns not corresponding to numbers?
     if cols is not None:
         data = data.iloc[:, cols]
-    data = data.apply(pandas.to_numeric, errors='coerce')
     for i in range(data.shape[1]):
         data.iloc[:, i] /= data.iloc[:, i].quantile(0.999)
     return data
