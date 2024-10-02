03f8d123adf7daf02be5ea0eb0927f91771bdf96
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-02 14:14:27 +0200

    Marked Shapley test as slow

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -87,6 +87,7 @@ def test_evaluate_model():
         assert len(stats[i]) == 5
 
 
+@pytest.mark.slow
 def test_shapley():
     X = pd.DataFrame(np.random.randn(100, 44))
     y = np.random.choice([0, 1], 100)
